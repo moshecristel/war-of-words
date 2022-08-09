21b33e704f633effe09973a40a4ae296d4228d28
Moshe Cristel <moshecristel@gmail.com>
2022-08-09 15:17:59 -0700

    Set up + added InputManager

---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,71 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+
+public class InputManager : MonoBehaviour
+{
+    public event Action<Vector2, Vector2> TouchStarted;
+    public event Action<Vector2, Vector2> TouchMoved;
+    public event Action<Vector2, Vector2> TouchEnded;
+
+    private Camera _cameraMain;
+
+    #region Lifecycle
+
+    private void Awake()
+    {
+        _cameraMain = Camera.main;
+    }
+
+    private void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();
+        TouchSimulation.Enable();
+        Touch.onFingerDown += TouchOnFingerDown;
+        Touch.onFingerUp += TouchOnFingerUp;
+        Touch.onFingerMove += TouchOnFingerMove;
+    }
+
+
+    private void OnDisable()
+    {
+        EnhancedTouchSupport.Disable();
+        TouchSimulation.Disable();
+        Touch.onFingerDown -= TouchOnFingerDown;
+        Touch.onFingerUp -= TouchOnFingerUp;
+        Touch.onFingerMove -= TouchOnFingerMove;
+    }
+    #endregion
+
+    #region Event Handlers
+    
+    private void TouchOnFingerDown(Finger finger)
+    {
+        Debug.Log("Finger DOWN: " + finger.currentTouch.screenPosition + "(began=" + finger.currentTouch.began + ")");
+        TouchStarted?.Invoke(finger.currentTouch.screenPosition, ScreenToWorldPosition(finger.currentTouch.screenPosition));
+    }
+    
+    private void TouchOnFingerMove(Finger finger)
+    {
+        Debug.Log("Finger MOVE: " + finger.currentTouch.screenPosition);
+        TouchMoved?.Invoke(finger.currentTouch.screenPosition, ScreenToWorldPosition(finger.currentTouch.screenPosition));
+    }
+    
+    private void TouchOnFingerUp(Finger finger)
+    {
+        Debug.Log("Finger UP: " + finger.currentTouch.screenPosition + "(ended=" + finger.currentTouch.ended + ")");
+        TouchEnded?.Invoke(finger.currentTouch.screenPosition, ScreenToWorldPosition(finger.currentTouch.screenPosition));
+    }
+    
+    #endregion
+
+    #region Methods
+
+    private Vector2 ScreenToWorldPosition(Vector2 screenPosition)
+    {
+        return _cameraMain.ScreenToWorldPoint(screenPosition);
+    }
+
+    #endregion
+}
