37f9a46998a0cfab8ee1383e91b39e0d66013932
Moshe Cristel <moshecristel@gmail.com>
2022-09-11 13:44:30 -0700

    Archived old code and fixed get pixel problem with 2D texture/sprite

--- Assets/Scripts/Game/Prep/Old/LibraryConsolidation.cs
+++ Assets/Scripts/Game/Prep/Old/LibraryConsolidation.cs
@@ -5,7 +5,6 @@ using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 using UnityEngine;
 
